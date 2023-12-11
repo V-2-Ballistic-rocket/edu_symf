ea0b6f874f7527314bc67c5c2b1c08f2e62d31f5
Петрушина Кристина Алексеевна <k.petrushina@etpgpb.ru>
2023-12-11 18:05:02 +0300

    add address register test

--- edu_symf/tests/Domain/Address/Factory/AddressFactoryTest.php
+++ edu_symf/tests/Domain/Address/Factory/AddressFactoryTest.php
@@ -2,12 +2,38 @@
 
 namespace App\Tests\Domain\Address\Factory;
 
+use App\DomainLayer\Address\AddressDTO\CreateAddressDTO;
+use App\DomainLayer\Address\Factory\AddressFactory;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AddressFactoryTest extends TestCase
 {
-    public function testSomething(): void
+    /**
+     * @dataProvider  validDataProvider
+     */
+    public function testSomething($country, $city, $street, $houseNumber): void
     {
-        $this->assertTrue(true);
+        $createAddressDTO = new CreateAddressDTO($country, $city, $street, $houseNumber);
+
+        $validator = $this->createMock(ValidatorInterface::class);
+        $validator->expects($this->once())->method('validate');
+        $address = $addressFactory = new AddressFactory($validator);
+        $this->assertEquals(
+            array($country, $city, $street, $houseNumber),
+            array($address->get)
+        );
+    }
+    public function validDataProvider():array
+    {
+        return [
+            'when data is valid' =>
+            [
+                'country' => 'Lapland',
+                'city' => 'NightCity',
+                'street' => 'RedLight',
+                'houseNumber' => '101'
+            ]
+        ];
     }
 }
