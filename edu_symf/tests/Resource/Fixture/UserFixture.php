ac14e704610a0f295af7b884b5b86778fdd61300
Петрушина Кристина Алексеевна <k.petrushina@etpgpb.ru>
2023-12-22 17:53:33 +0300

    add fixture ver1

---
+++ edu_symf/tests/Resource/Fixture/UserFixture.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Resource\Fixture;
+
+use App\InfrastructureLayer\Postgres\Entity\Address;
+use App\InfrastructureLayer\Postgres\Entity\Profile;
+use App\InfrastructureLayer\Postgres\Entity\Users;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Uid\Uuid;
+
+class UserFixture extends Fixture
+{
+    const REFERENCE = 'user';
+    public function load(ObjectManager $manager)
+    {
+        $addressId = Uuid::V1();
+        $address = new Address(
+            $addressId,
+            'Testandia',
+            'Testsantos',
+            'Teststreet',
+            '64/Test'
+        );
+        $profileId = Uuid::v1();
+        $profile = new Profile(
+            $profileId,
+            'Testivald',
+            'Testov',
+            64,
+            'to_avatar_test_path'
+        );
+        $userId = Uuid::v1();
+        $userToken = Uuid::v1();
+        $user = new Users(
+            $userId,
+            'test_login',
+            'test_password',
+            'test@email.com',
+            '88005553535',
+            $addressId,
+            $profileId,
+            $userToken
+        );
+        $manager->persist($address);
+        $manager->persist($profile);
+        $manager->persist($user);
+        $manager->flush();
+        $this->addReference(self::REFERENCE, $user);
+    }
+}
